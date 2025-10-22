b3db38562aef17053f4b830fcdf74ec39588aa34
MedYassineWerdani <mohamedyassine.elwerdani@polytechnicien.tn>
2025-10-22 23:26:56 +0100

    changes in eda exp

--- eda.py
+++ eda.py
@@ -2,5 +2,5 @@ import pandas as pd
 df = pd.read_csv('data.csv')
 print(df.head())
 
-#MAKING THE WORLD A BETTER PLACE WITH GOOD CODE :) SDSQEA
+#MAKING THE WORLD A BETTER PLACE WITH GOOD CODE :) CONFLIUCTS HAHAHHAHAh
 print("Data loaded successfully!")
